957c27460a5f43eae44bcc1aebbac10e10dead39
Baudi <baudilio.huanca@outlook.com>
2017-12-11 23:49:29 -0400

    Basic conversation with messages

--- PipeClient/Program.cs
+++ PipeClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,102 @@ namespace PipeClient
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Client Running");
+            ConversationWithTheServer();
         }
+
+        private static void ReceiveByteAndRespond()
+        {
+            using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream("test-pipe"))
+            {
+                namedPipeClient.Connect();
+                Console.WriteLine(namedPipeClient.ReadByte());
+                namedPipeClient.WriteByte(2);
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void ReceiveByteAndRespondContinuous()
+        {
+            using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream("test-pipe"))
+            {
+                namedPipeClient.Connect();
+                Console.WriteLine("Client connected to the named pipe server. Waiting for server to send first byte...");
+                Console.WriteLine("The server sent a single byte to the client: {0}", namedPipeClient.ReadByte());
+                Console.Write("Provide a byte response from client: ");
+                string b = Console.ReadLine();
+                Console.WriteLine("About to send byte {0} to server.", b);
+                namedPipeClient.WriteByte(Encoding.UTF8.GetBytes(b).First());
+                while (b != "x")
+                {
+                    Console.WriteLine("The server sent a single byte to the client: {0}", namedPipeClient.ReadByte());
+                    Console.Write("Provide a byte response from client: ");
+                    b = Console.ReadLine();
+                    Console.WriteLine("About to send byte {0} to server.", b);
+                    namedPipeClient.WriteByte(Encoding.UTF8.GetBytes(b).First());
+                }
+
+                Console.WriteLine("Client chose to disconnect...");
+            }
+        }
+
+        private static void SendSingleMessageToServer()
+        {
+            using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream("test-pipe"))
+            {
+                Console.Write("Enter a message to be sent to the server: ");
+                string message = Console.ReadLine();
+                namedPipeClient.Connect();
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                namedPipeClient.Write(messageBytes, 0, messageBytes.Length);
+            }
+        }
+
+        #region basic conversation with messages
+        private static void ConversationWithTheServer()
+        {
+            using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream(".", "test-pipe", PipeDirection.InOut))
+            {
+                namedPipeClient.Connect();
+                Console.WriteLine("Client connected to the named pipe server. Waiting for server to send the first message...");
+
+                namedPipeClient.ReadMode = PipeTransmissionMode.Message;
+                string messageFromServer = ProcessSingleReceivedMessage(namedPipeClient);
+                Console.WriteLine("The server is saying {0}", messageFromServer);
+
+                Console.Write("Write a response: ");
+                string response = Console.ReadLine();
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                namedPipeClient.Write(responseBytes, 0, responseBytes.Length);
+
+                while (response != "x")
+                {
+                    messageFromServer = ProcessSingleReceivedMessage(namedPipeClient);
+                    Console.WriteLine("The server is saying {0}", messageFromServer);
+                    Console.Write("Write a response: ");
+                    response = Console.ReadLine();
+                    responseBytes = Encoding.UTF8.GetBytes(response);
+                    namedPipeClient.Write(responseBytes, 0, responseBytes.Length);
+                }
+            }
+        }
+
+        private static string ProcessSingleReceivedMessage(NamedPipeClientStream namedPipeClient)
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[5];
+            do
+            {
+                namedPipeClient.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+            }
+            while (!namedPipeClient.IsMessageComplete);
+            return messageBuilder.ToString();
+        }
+        #endregion
     }
 }
--- PipeServer/Program.cs
+++ PipeServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,121 @@ namespace PipeServer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Server Running");
+            ConversationWithTheClient();
+        }
+
+        private static void SendByteAndReceiveResponse()
+        {
+            using (NamedPipeServerStream namedPipeServer = new NamedPipeServerStream("test-pipe"))
+            {
+                namedPipeServer.WaitForConnection();
+                namedPipeServer.WriteByte(1);
+                int byteFromClient = namedPipeServer.ReadByte();
+                Console.WriteLine(byteFromClient);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void SendByteAndReceiveResponseContinuous()
+        {
+            using (NamedPipeServerStream namedPipeServer = new NamedPipeServerStream("test-pipe"))
+            {
+                Console.WriteLine("Server waiting for a connection...");
+                namedPipeServer.WaitForConnection();
+
+                Console.Write("A client has connected, send a byte from the server: ");
+                string b = Console.ReadLine();
+                Console.WriteLine("About to send byte {0} to client.", b);
+                namedPipeServer.WriteByte(Encoding.UTF8.GetBytes(b).First());
+
+                Console.WriteLine("Byte sent, waiting for response from client...");
+                int byteFromClient = namedPipeServer.ReadByte();
+
+                Console.WriteLine("Received byte response from client: {0}", byteFromClient);
+                while (byteFromClient != 120)
+                {
+                    Console.Write("Send a byte response: ");
+                    b = Console.ReadLine();
+                    Console.WriteLine("About to send byte {0} to client.", b);
+                    namedPipeServer.WriteByte(Encoding.UTF8.GetBytes(b).First());
+                    Console.WriteLine("Byte sent, waiting for response from client...");
+                    byteFromClient = namedPipeServer.ReadByte();
+                    Console.WriteLine("Received byte response from client: {0}", byteFromClient);
+                }
+                Console.WriteLine("Server exiting, client sent an 'x'...");
+            }
+        }
+
+        private static void ReceiveSingleMessageFromClient()
+        {
+            using (NamedPipeServerStream namedPipeServer = new NamedPipeServerStream("test-pipe", PipeDirection.InOut,
+                1, PipeTransmissionMode.Message))
+            {
+                namedPipeServer.WaitForConnection();
+                StringBuilder messageBuilder = new StringBuilder();
+                string messageChunk = string.Empty;
+                byte[] messageBuffer = new byte[5];
+                do
+                {
+                    namedPipeServer.Read(messageBuffer, 0, messageBuffer.Length);
+                    messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                    messageBuilder.Append(messageChunk);
+                    messageBuffer = new byte[messageBuffer.Length];
+                }
+                while (!namedPipeServer.IsMessageComplete);
+
+                Console.WriteLine(messageBuilder.ToString());
+            }
+        }
+
+        #region basic conversation with messages
+        private static void ConversationWithTheClient()
+        {
+            using (NamedPipeServerStream namedPipeServer = new NamedPipeServerStream("test-pipe", PipeDirection.InOut,
+                1, PipeTransmissionMode.Message))
+            {
+                Console.WriteLine("Server waiting for a connection...");
+                namedPipeServer.WaitForConnection();
+
+                Console.Write("A client has connected, send a greeting from the server: ");
+                string message = Console.ReadLine();
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                namedPipeServer.Write(messageBytes, 0, messageBytes.Length);
+
+                string response = ProcessSingleReceivedMessage(namedPipeServer);
+                Console.WriteLine("The client has responded: {0}", response);
+                while (response != "x")
+                {
+                    Console.Write("Send a response from the server: ");
+                    message = Console.ReadLine();
+                    messageBytes = Encoding.UTF8.GetBytes(message);
+                    namedPipeServer.Write(messageBytes, 0, messageBytes.Length);
+                    response = ProcessSingleReceivedMessage(namedPipeServer);
+                    Console.WriteLine("The client is saying {0}", response);
+                }
+
+                Console.WriteLine("The client has ended the conversation.");
+            }
+        }
+
+        private static string ProcessSingleReceivedMessage(NamedPipeServerStream namedPipeServer)
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[5];
+            do
+            {
+                namedPipeServer.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+            }
+            while (!namedPipeServer.IsMessageComplete);
+
+            return messageBuilder.ToString();
         }
     }
+    #endregion
 }
